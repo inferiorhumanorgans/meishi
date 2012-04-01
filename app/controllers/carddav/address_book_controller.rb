33433c339657c8c47f7db61cec17aafc0991fd66
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-04-01 12:04:09 -0700

    Move the addressbook-multiget report into its own function.

--- app/controllers/carddav/address_book_controller.rb
+++ app/controllers/carddav/address_book_controller.rb
@@ -7,39 +7,46 @@ module Carddav
         return NotFound
       end
 
-      c_ns = ns('urn:ietf:params:xml:ns:carddav')
-
-      Rails.logger.error request_document.to_xml
-      if not request_document.xpath("/#{c_ns}addressbook-multiget").empty?
-        Rails.logger.error "REPORT addressbook-multiget"
-        props = request_document.xpath("/#{c_ns}addressbook-multiget/#{ns}prop").children.find_all{|n| n.element?}.map{|n|
-          {:name => n.name, :namespace => n.namespace.prefix, :ns_href => n.namespace.href}
-        }
-        hrefs = request_document.xpath("/#{c_ns}addressbook-multiget/#{ns}href").collect{|n| 
-          text = n.text
-          path = URI.parse(text).path
-          Rails.logger.error "Scanned this HREF: #{text} PATH: #{path}"
-          text
-        }.compact
-
-        multistatus do |xml|
-          hrefs.each do |_href|
-            xml.response do
-              xml.href _href
-              path = File.split(URI.parse(_href).path).last
-              Rails.logger.error "Creating child w/ ORIG=#{resource.public_path} HREF=#{_href} FILE=#{path}!"
-              if resource.public_path == _href
-                propstats(xml, get_properties(resource, props))
-              else
-                propstats(xml, get_properties(resource.child(File.split(path).last), props))
-              end
-            end
-          end
-        end
+      Rails.logger.error "REPORT XML REQUEST:\n#{request_document.to_xml}"
+      case request_document.root.name
+      when 'addressbook-multiget'
+        addressbook_multiget
       else
         NotAcceptable
       end
 
+    end
+
+    protected
+    def addressbook_multiget
+      Rails.logger.error "REPORT addressbook-multiget"
+
+      c_ns = ns('urn:ietf:params:xml:ns:carddav')
+
+      props = request_document.xpath("/#{c_ns}addressbook-multiget/#{ns}prop").children.find_all{|n| n.element?}.map{|n|
+        {:name => n.name, :namespace => n.namespace.prefix, :ns_href => n.namespace.href}
+      }
+      hrefs = request_document.xpath("/#{c_ns}addressbook-multiget/#{ns}href").collect{|n| 
+        text = n.text
+        path = URI.parse(text).path
+        Rails.logger.error "Scanned this HREF: #{text} PATH: #{path}"
+        text
+      }.compact
+
+      multistatus do |xml|
+        hrefs.each do |_href|
+          xml.response do
+            xml.href _href
+            path = File.split(URI.parse(_href).path).last
+            Rails.logger.error "Creating child w/ ORIG=#{resource.public_path} HREF=#{_href} FILE=#{path}!"
+            if resource.public_path == _href
+              propstats(xml, get_properties(resource, props))
+            else
+              propstats(xml, get_properties(resource.child(File.split(path).last), props))
+            end
+          end
+        end
+      end
     end
 
   end
