b9c559c366cc66b171b823cc5ab22dad3a1e0900
Alex Zepeda <alex@inferiorhumanorgans.com>
2012-04-10 13:05:57 -0700

    Add an xml_error function for more verbose error messages.

--- app/controllers/carddav/base_controller.rb
+++ app/controllers/carddav/base_controller.rb
@@ -22,6 +22,18 @@ module Carddav
     
     protected
 
+    # Takes a block, gives it an xml builder with a DAV:error root element, and
+    # will render the XML body and set an HTTP error code.  The HTTP status is
+    # 403-Forbidden by default as per WebDAV's suggestions, alternatively, 409
+    # a.k.a. Conflict is suggested for errors that are transient.  Any HTTPStatus
+    # class will work.  Because this triggers an exception, it won't return.
+    def xml_error(http_error_code=Forbidden, &block)
+      render_xml(:error) do |xml|
+        block.yield(xml)
+      end
+      raise http_error_code
+    end
+
     def xpath_element(name, ns_uri=:dav)
       case ns_uri
       when :dav
