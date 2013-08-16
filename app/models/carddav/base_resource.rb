inferiorhumanorgans/meishi

commit cbc8f5cf420898b9bf751f3afaf0a5a65491b042

General formatting cleanup.
Signed-off-by: Alex Zepeda <alex@inferiorhumanorgans.com>

app/models/carddav/base_resource.rb
@@ -1,231 +1,229 @@
-module Carddav
-  class BaseResource < DAV4Rack::Resource
-    # On the subclass define a similar hash for properties specific to that
-    # subclass, and another hash for properties specific to that subclass that
-    # should not be returned in an allprop request.
-    # Properties defined here may be implemented in the subclass, but do not
-    # need to be defined there
-    BASE_PROPERTIES = {
-      'DAV:' => %w(
-        acl
-        acl-restrictions
-        creationdate
-        current-user-principal
-        current-user-privilege-set
-        displayname
-        getcontentlength
-        getcontenttype
-        getetag
-        getlastmodified
-        group
-        owner
-        principal-URL
-        resourcetype
-      ),
-      # Define this here as an empty array so it will fall through to dav4rack
-      # and they'll return a NotImplemented instead of BadRequest
-      'urn:ietf:params:xml:ns:carddav' => []
-    }
+class Carddav::BaseResource < DAV4Rack::Resource
+  # On the subclass define a similar hash for properties specific to that
+  # subclass, and another hash for properties specific to that subclass that
+  # should not be returned in an allprop request.
+  # Properties defined here may be implemented in the subclass, but do not
+  # need to be defined there
+  BASE_PROPERTIES = {
+    'DAV:' => %w(
+      acl
+      acl-restrictions
+      creationdate
+      current-user-principal
+      current-user-privilege-set
+      displayname
+      getcontentlength
+      getcontenttype
+      getetag
+      getlastmodified
+      group
+      owner
+      principal-URL
+      resourcetype
+    ),
+    # Define this here as an empty array so it will fall through to dav4rack
+    # and they'll return a NotImplemented instead of BadRequest
+    'urn:ietf:params:xml:ns:carddav' => []
+  }
 
-    # Define a convenience function for CalDAV properties.  This will define
-    # a function named after the first argument (a symbol) that populates three
-    # instance variables: @attributes, @children, and @attribute. Note that
-    # next should be used inside of blocks instead of return.
-    def self.prop(method, options={}, &b)
-      self.class_eval do
-        define_method(method) do |attributes={}, children=[]|
-          self.instance_variable_set(:@attributes, attributes)
-          self.instance_variable_set(:@children, children)
-          self.instance_variable_set(:@attribute, method)
+  # Define a convenience function for CalDAV properties.  This will define
+  # a function named after the first argument (a symbol) that populates three
+  # instance variables: @attributes, @children, and @attribute. Note that
+  # next should be used inside of blocks instead of return.
+  def self.prop(method, options={}, &b)
+    self.class_eval do
+      define_method(method) do |attributes={}, children=[]|
+        self.instance_variable_set(:@attributes, attributes)
+        self.instance_variable_set(:@children, children)
+        self.instance_variable_set(:@attribute, method)
 
-          unless options[:args] == true
-            unexpected_arguments(attributes, children)
-          end
-
-          self.instance_exec &b
+        unless options[:args] == true
+          unexpected_arguments(attributes, children)
         end
 
-        # DAV4Rack will clobber public methods, so let's make sure these are all protected.
-        protected method
-      end
-    end
-
-    PRIVILEGES = %w(read read-acl read-current-user-privilege-set)
-
-    # Make OSX's AddressBook.app happy :(
-    def setup
-      @propstat_relative_path = true
-      @root_xml_attributes = {
-        'xmlns:C' => 'urn:ietf:params:xml:ns:carddav', 
-        'xmlns:APPLE1' => 'http://calendarserver.org/ns/'
-      }
-    end
-
-    def warden
-      request.env['warden']
-    end
-
-    def current_user
-      @current_user ||= warden.authenticate(:scope => :user)
-      @current_user
-    end
-
-    def is_self?(other_path)
-      ary = [@public_path]
-      ary.push(@public_path+'/') if @public_path[-1] != '/'
-      ary.push(@public_path[0..-2]) if @public_path[-1] == '/'
-      ary.include? other_path
-    end
-
-    def get_property(element)
-      name = element[:name]
-      namespace = element[:ns_href]
-
-      begin
-        our_properties = BaseResource.merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
-        our_properties = BaseResource.merge_properties(our_properties, self.class::EXPLICIT_PROPERTIES)
-      rescue
-        # Just in case we don't have any properties defined on the subclass
-        our_properties = BASE_PROPERTIES
+        self.instance_exec &b
       end
 
-      unless our_properties.include? namespace
-        raise BadRequest
-      end
-
-      fn = name.underscore
-
-      if our_properties[namespace].include?(name)
-        # The base dav4rack handler will use nicer looking function names for some properties
-        # Let's just humor it.  If we don't define a local prop_foo method, fall back to the
-        # super class's implementation of get_property which we hope will handle our request.
-        if self.respond_to?(fn)
-          if element[:children].empty? and element[:attributes].empty?
-            return self.send(fn.to_sym)
-          else
-            return self.send(fn.to_sym, element[:attributes], element[:children])
-          end
-        end
-      end
-
-      super(element)
+      # DAV4Rack will clobber public methods, so let's make sure these are all protected.
+      protected method
     end
-
-    # Some properties shouldn't be included in an allprop request
-    # but it's nice to do some sanity checking so keeping a list is good
-    def properties
-      BaseResource::merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
-    end
-
-    # Properties in alphabetical order
-    # Properties need to be protected so that dav4rack doesn't alias them away
-    protected
-
-    # Let's implement the simplest policy possible... modifying the permissions
-    # via WebDAV is not supported (yet?).
-    # TODO: Articulate permissions here for all users as part of a proper admin implementation
-    # TODO: Offer up unique principal URIs on a per-user basis
-    prop :acl do
-      s="
-      <D:acl xmlns:D='DAV:'>
-        <D:ace>
-          <D:principal>
-            <D:href>#{url_or_path(:principal)}</D:href>
-          </D:principal>
-          <D:protected/>
-          <D:grant>
-          %s
-          </D:grant>
-        </D:ace>
-      </D:acl>"
-      s %= get_privileges_aggregate
-      Nokogiri::XML::DocumentFragment.parse(s)      
-    end
-
-    prop :acl_restrictions do
-      s="<D:acl-restrictions xmlns:D='DAV:'><D:grant-only/><D:no-invert/></D:acl-restrictions>"
-      Nokogiri::XML::DocumentFragment.parse(s)
-    end
-
-    # This violates the spec that requires an HTTP or HTTPS URL.  Unfortunately,
-    # Apple's AddressBook.app treats everything as a pathname.  Also, the model
-    # shouldn't need to know about the URL scheme and such.
-    prop :current_user_principal do
-      s="<D:current-user-principal xmlns:D='DAV:'><D:href>#{url_or_path(:principal)}</D:href></D:current-user-principal>"
-      Nokogiri::XML::DocumentFragment.parse(s)
-    end
-
-    prop :current_user_privilege_set do
-      s='<D:current-user-privilege-set xmlns:D="DAV:">%s</D:current-user-privilege-set>'
-
-      s %= get_privileges_aggregate
-      Nokogiri::XML::DocumentFragment.parse(s)
-    end
-
-    prop :group do
-    end
-
-    prop :owner do
-      s="<D:owner xmlns:D='DAV:'><D:href>#{url_or_path(:principal)}</D:href></D:owner>"
-      Nokogiri::XML::DocumentFragment.parse(s)
-    end
-
-    prop :principal_url do
-      s="<D:principal-URL xmlns:D='DAV:'><D:href>#{url_or_path(:principal)}</D:href></D:principal-URL>"
-      Nokogiri::XML::DocumentFragment.parse(s)
-    end
-
-    # These are not properties.
-    protected
-    def self.merge_properties(all, explicit)
-      ret = all.dup
-      explicit.each do |key, value|
-        ret[key] ||= []
-        ret[key] += value
-        ret[key].uniq!
-      end
-      ret
-    end
-
-    # Call this so that we log requests with unexepcted extra fluff
-    def unexpected_arguments(attributes, children)
-      return if (attributes.nil? or attributes.empty?) and (children.nil? or children.empty?)
-
-      Rails.logger.error "#{@attribute} request did not expect arguments: #{attributes.inspect} / #{children.inspect}"
-    end
-
-    # Default URL builder options -- we need these because we're doing bad things by calling
-    # the URL helpers from outside the view context.
-    def url_options
-      {host: request.host, port: request.port, protocol: request.scheme}
-    end
-
-    # So, for our first quirk: MacOS 10.6 needs paths and not URLs (it will treat URLs as paths...)
-    def url_or_path(route_name, fluff={})
-      method = nil
-
-      if Quirks[:CURRENT_PRINCIPAL_NO_URL]
-        Quirks[:CURRENT_PRINCIPAL_NO_URL].each do |user_agent_match|
-          if Regexp.new(user_agent_match) =~ request.env['HTTP_USER_AGENT']
-            method = (route_name.to_s + '_path').to_sym
-            break
-          end
-        end
-      end
-
-      method ||= (route_name.to_s + '_url').to_sym
-      options = []
-      options << fluff.delete(:object) if fluff.include? :object
-      options << url_options.merge(fluff)
-      Rails.application.routes.url_helpers.send(method, *options)
-    end
-
-    private
-    def get_privileges_aggregate
-      privileges_aggregate = PRIVILEGES.inject('') do |ret, priv|
-        ret << '<D:privilege><%s /></privilege>' % priv
-      end
-    end
-
   end
+
+  PRIVILEGES = %w(read read-acl read-current-user-privilege-set)
+
+  # Make OSX's AddressBook.app happy :(
+  def setup
+    @propstat_relative_path = true
+    @root_xml_attributes = {
+      'xmlns:C' => 'urn:ietf:params:xml:ns:carddav', 
+      'xmlns:APPLE1' => 'http://calendarserver.org/ns/'
+    }
+  end
+
+  def warden
+    request.env['warden']
+  end
+
+  def current_user
+    @current_user ||= warden.authenticate(:scope => :user)
+    @current_user
+  end
+
+  def is_self?(other_path)
+    ary = [@public_path]
+    ary.push(@public_path+'/') if @public_path[-1] != '/'
+    ary.push(@public_path[0..-2]) if @public_path[-1] == '/'
+    ary.include? other_path
+  end
+
+  def get_property(element)
+    name = element[:name]
+    namespace = element[:ns_href]
+
+    begin
+      our_properties = BaseResource.merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
+      our_properties = BaseResource.merge_properties(our_properties, self.class::EXPLICIT_PROPERTIES)
+    rescue
+      # Just in case we don't have any properties defined on the subclass
+      our_properties = BASE_PROPERTIES
+    end
+
+    unless our_properties.include? namespace
+      raise BadRequest
+    end
+
+    fn = name.underscore
+
+    if our_properties[namespace].include?(name)
+      # The base dav4rack handler will use nicer looking function names for some properties
+      # Let's just humor it.  If we don't define a local prop_foo method, fall back to the
+      # super class's implementation of get_property which we hope will handle our request.
+      if self.respond_to?(fn)
+        if element[:children].empty? and element[:attributes].empty?
+          return self.send(fn.to_sym)
+        else
+          return self.send(fn.to_sym, element[:attributes], element[:children])
+        end
+      end
+    end
+
+    super(element)
+  end
+
+  # Some properties shouldn't be included in an allprop request
+  # but it's nice to do some sanity checking so keeping a list is good
+  def properties
+    BaseResource::merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
+  end
+
+  # Properties in alphabetical order
+  # Properties need to be protected so that dav4rack doesn't alias them away
+  protected
+
+  # Let's implement the simplest policy possible... modifying the permissions
+  # via WebDAV is not supported (yet?).
+  # TODO: Articulate permissions here for all users as part of a proper admin implementation
+  # TODO: Offer up unique principal URIs on a per-user basis
+  prop :acl do
+    s="
+    <D:acl xmlns:D='DAV:'>
+      <D:ace>
+        <D:principal>
+          <D:href>#{url_or_path(:principal)}</D:href>
+        </D:principal>
+        <D:protected/>
+        <D:grant>
+        %s
+        </D:grant>
+      </D:ace>
+    </D:acl>"
+    s %= get_privileges_aggregate
+    Nokogiri::XML::DocumentFragment.parse(s)      
+  end
+
+  prop :acl_restrictions do
+    s="<D:acl-restrictions xmlns:D='DAV:'><D:grant-only/><D:no-invert/></D:acl-restrictions>"
+    Nokogiri::XML::DocumentFragment.parse(s)
+  end
+
+  # This violates the spec that requires an HTTP or HTTPS URL.  Unfortunately,
+  # Apple's AddressBook.app treats everything as a pathname.  Also, the model
+  # shouldn't need to know about the URL scheme and such.
+  prop :current_user_principal do
+    s="<D:current-user-principal xmlns:D='DAV:'><D:href>#{url_or_path(:principal)}</D:href></D:current-user-principal>"
+    Nokogiri::XML::DocumentFragment.parse(s)
+  end
+
+  prop :current_user_privilege_set do
+    s='<D:current-user-privilege-set xmlns:D="DAV:">%s</D:current-user-privilege-set>'
+
+    s %= get_privileges_aggregate
+    Nokogiri::XML::DocumentFragment.parse(s)
+  end
+
+  prop :group do
+  end
+
+  prop :owner do
+    s="<D:owner xmlns:D='DAV:'><D:href>#{url_or_path(:principal)}</D:href></D:owner>"
+    Nokogiri::XML::DocumentFragment.parse(s)
+  end
+
+  prop :principal_url do
+    s="<D:principal-URL xmlns:D='DAV:'><D:href>#{url_or_path(:principal)}</D:href></D:principal-URL>"
+    Nokogiri::XML::DocumentFragment.parse(s)
+  end
+
+  # These are not properties.
+  protected
+  def self.merge_properties(all, explicit)
+    ret = all.dup
+    explicit.each do |key, value|
+      ret[key] ||= []
+      ret[key] += value
+      ret[key].uniq!
+    end
+    ret
+  end
+
+  # Call this so that we log requests with unexepcted extra fluff
+  def unexpected_arguments(attributes, children)
+    return if (attributes.nil? or attributes.empty?) and (children.nil? or children.empty?)
+
+    Rails.logger.error "#{@attribute} request did not expect arguments: #{attributes.inspect} / #{children.inspect}"
+  end
+
+  # Default URL builder options -- we need these because we're doing bad things by calling
+  # the URL helpers from outside the view context.
+  def url_options
+    {host: request.host, port: request.port, protocol: request.scheme}
+  end
+
+  # So, for our first quirk: MacOS 10.6 needs paths and not URLs (it will treat URLs as paths...)
+  def url_or_path(route_name, fluff={})
+    method = nil
+
+    if Quirks[:CURRENT_PRINCIPAL_NO_URL]
+      Quirks[:CURRENT_PRINCIPAL_NO_URL].each do |user_agent_match|
+        if Regexp.new(user_agent_match) =~ request.env['HTTP_USER_AGENT']
+          method = (route_name.to_s + '_path').to_sym
+          break
+        end
+      end
+    end
+
+    method ||= (route_name.to_s + '_url').to_sym
+    options = []
+    options << fluff.delete(:object) if fluff.include? :object
+    options << url_options.merge(fluff)
+    Rails.application.routes.url_helpers.send(method, *options)
+  end
+
+  private
+  def get_privileges_aggregate
+    privileges_aggregate = PRIVILEGES.inject('') do |ret, priv|
+      ret << '<D:privilege><%s /></privilege>' % priv
+    end
+  end
+
 end

app/controllers/carddav/address_book_controller.rb
@@ -36,92 +36,88 @@ RFC 6352
     Section 10.4) specify a media type supported by the server for
     address object resources.
 =end
-module Carddav
+class Carddav::AddressBookController < Carddav::BaseController
 
-  class AddressBookController < BaseController
-
-    def report
-      unless resource.exist?
-        return NotFound
-      end
-
-      Rails.logger.error "REPORT XML REQUEST:\n#{request_document.to_xml}"
-      Rails.logger.error "REPORT DEPTH IS: #{depth.inspect}"
-
-
-      if request_document.nil? or request_document.root.nil?
-        xml_error(BadRequest) do |err|
-          err.send :'empty-request'
-        end
-      end
-
-      case request_document.root.name
-      when 'addressbook-multiget'
-        addressbook_multiget
-      else
-        xml_error do |err|
-          err.send :'supported-report'
-        end
-      end
-
+  def report
+    unless resource.exist?
+      return NotFound
     end
 
-    protected
-    def addressbook_multiget
-      Rails.logger.error "REPORT addressbook-multiget"
+    Rails.logger.error "REPORT XML REQUEST:\n#{request_document.to_xml}"
+    Rails.logger.error "REPORT DEPTH IS: #{depth.inspect}"
 
-      # TODO: Include a DAV:error response
-      # CardDAV §8.7 clearly states Depth must equal zero for this report
-      # But Apple's AddressBook.app sets the depth to infinity anyhow.
-      unless depth == 0 or depth == :infinity
-        xml_error(BadRequest) do |err|
-          err.send :'invalid-depth'
-        end
+
+    if request_document.nil? or request_document.root.nil?
+      xml_error(BadRequest) do |err|
+        err.send :'empty-request'
       end
+    end
 
-      props = request_document.xpath("/#{xpath_element('addressbook-multiget', :carddav)}/#{xpath_element('prop')}").children.find_all{|n| n.element?}.map{|n|
-        to_element_hash(n)
-      }
-      # Handle the address-data element
-      # - Check for child properties (vCard fields)
-      # - Check for mime-type and version.  If present they must match vCard 3.0 for now since we don't support anything else.
-      hrefs = request_document.xpath("/#{xpath_element('addressbook-multiget', :carddav)}/#{xpath_element('href')}").collect{|n| 
-        text = n.text
-        # TODO: Make sure that the hrefs passed into the report are either paths or fully qualified URLs with the right host+protocol+port prefix
-        path = URI.parse(text).path
-        Rails.logger.error "Scanned this HREF: #{text} PATH: #{path}"
-        text
-      }.compact
-      
-      if hrefs.empty?
-        xml_error(BadRequest) do |err|
-          err.send :'href-missing'
-        end
+    case request_document.root.name
+    when 'addressbook-multiget'
+      addressbook_multiget
+    else
+      xml_error do |err|
+        err.send :'supported-report'
       end
+    end
 
-      multistatus do |xml|
-        hrefs.each do |_href|
-          xml.response do
-            xml.href _href
+  end
 
-            path = File.split(URI.parse(_href).path).last
-            Rails.logger.error "Creating child w/ ORIG=#{resource.public_path} HREF=#{_href} FILE=#{path}!"
+  protected
+  def addressbook_multiget
+    Rails.logger.error "REPORT addressbook-multiget"
 
-            # TODO: Write a test to cover asking for a report expecting contact objects but given an address book path
-            # Yes, CardDAVMate does this.
-            cur_resource = resource.is_self?(_href) ? resource : resource.child(File.split(path).last)
-            
-            if cur_resource.exist?
-              propstats(xml, get_properties(cur_resource, props))
-            else
-              xml.status "#{http_version} #{NotFound.status_line}"
-            end
+    # TODO: Include a DAV:error response
+    # CardDAV §8.7 clearly states Depth must equal zero for this report
+    # But Apple's AddressBook.app sets the depth to infinity anyhow.
+    unless depth == 0 or depth == :infinity
+      xml_error(BadRequest) do |err|
+        err.send :'invalid-depth'
+      end
+    end
 
+    props = request_document.xpath("/#{xpath_element('addressbook-multiget', :carddav)}/#{xpath_element('prop')}").children.find_all{|n| n.element?}.map{|n|
+      to_element_hash(n)
+    }
+    # Handle the address-data element
+    # - Check for child properties (vCard fields)
+    # - Check for mime-type and version.  If present they must match vCard 3.0 for now since we don't support anything else.
+    hrefs = request_document.xpath("/#{xpath_element('addressbook-multiget', :carddav)}/#{xpath_element('href')}").collect{|n| 
+      text = n.text
+      # TODO: Make sure that the hrefs passed into the report are either paths or fully qualified URLs with the right host+protocol+port prefix
+      path = URI.parse(text).path
+      Rails.logger.error "Scanned this HREF: #{text} PATH: #{path}"
+      text
+    }.compact
+    
+    if hrefs.empty?
+      xml_error(BadRequest) do |err|
+        err.send :'href-missing'
+      end
+    end
+
+    multistatus do |xml|
+      hrefs.each do |_href|
+        xml.response do
+          xml.href _href
+
+          path = File.split(URI.parse(_href).path).last
+          Rails.logger.error "Creating child w/ ORIG=#{resource.public_path} HREF=#{_href} FILE=#{path}!"
+
+          # TODO: Write a test to cover asking for a report expecting contact objects but given an address book path
+          # Yes, CardDAVMate does this.
+          cur_resource = resource.is_self?(_href) ? resource : resource.child(File.split(path).last)
+          
+          if cur_resource.exist?
+            propstats(xml, get_properties(cur_resource, props))
+          else
+            xml.status "#{http_version} #{NotFound.status_line}"
           end
+
         end
       end
     end
-
   end
 
 end

app/controllers/carddav/base_controller.rb
@@ -1,49 +1,45 @@
-module Carddav
+class Carddav::BaseController < DAV4Rack::Controller
 
-  class BaseController < DAV4Rack::Controller
-    
-    NAMESPACES = %w(urn:ietf:params:xml:ns:carddav DAV:)
+  NAMESPACES = %w(urn:ietf:params:xml:ns:carddav DAV:)
 
-    # TODO: Should really move towards inheriting from the proper rails classes
-    # Anyhow the intent here is to ensure that the DAV header gets set for every
-    # request under our purview.  Yes, a before_filter would be cleaner.
-    # Apparently AddressBook.app will skip its OPTIONS request if it sees the DAV
-    # header.  Neat.
-    def initialize(request, response, options={})
-      super
-      @response["DAV"] = "1, 2, access-control, addressbook"
+  # TODO: Should really move towards inheriting from the proper rails classes
+  # Anyhow the intent here is to ensure that the DAV header gets set for every
+  # request under our purview.  Yes, a before_filter would be cleaner.
+  # Apparently AddressBook.app will skip its OPTIONS request if it sees the DAV
+  # header.  Neat.
+  def initialize(request, response, options={})
+    super
+    @response["DAV"] = "1, 2, access-control, addressbook"
+  end
+
+  # Return response to OPTIONS
+  def options
+    @response["Allow"] = 'OPTIONS,HEAD,GET,PUT,POST,DELETE,PROPFIND,PROPPATCH,MKCOL,COPY,MOVE,LOCK,UNLOCK'
+    OK
+  end
+
+  protected
+
+  # Takes a block, gives it an xml builder with a DAV:error root element, and
+  # will render the XML body and set an HTTP error code.  The HTTP status is
+  # 403-Forbidden by default as per WebDAV's suggestions, alternatively, 409
+  # a.k.a. Conflict is suggested for errors that are transient.  Any HTTPStatus
+  # class will work.  Because this triggers an exception, it won't return.
+  def xml_error(http_error_code=Forbidden, &block)
+    render_xml(:error) do |xml|
+      block.yield(xml)
     end
+    raise http_error_code
+  end
 
-    # Return response to OPTIONS
-    def options
-      @response["Allow"] = 'OPTIONS,HEAD,GET,PUT,POST,DELETE,PROPFIND,PROPPATCH,MKCOL,COPY,MOVE,LOCK,UNLOCK'
-      OK
+  def xpath_element(name, ns_uri=:dav)
+    case ns_uri
+    when :dav
+      ns_uri = 'DAV:'
+    when :carddav
+      ns_uri = 'urn:ietf:params:xml:ns:carddav'
     end
-    
-    protected
-
-    # Takes a block, gives it an xml builder with a DAV:error root element, and
-    # will render the XML body and set an HTTP error code.  The HTTP status is
-    # 403-Forbidden by default as per WebDAV's suggestions, alternatively, 409
-    # a.k.a. Conflict is suggested for errors that are transient.  Any HTTPStatus
-    # class will work.  Because this triggers an exception, it won't return.
-    def xml_error(http_error_code=Forbidden, &block)
-      render_xml(:error) do |xml|
-        block.yield(xml)
-      end
-      raise http_error_code
-    end
-
-    def xpath_element(name, ns_uri=:dav)
-      case ns_uri
-      when :dav
-        ns_uri = 'DAV:'
-      when :carddav
-        ns_uri = 'urn:ietf:params:xml:ns:carddav'
-      end
-      "*[local-name()='#{name}' and namespace-uri()='#{ns_uri}']"
-    end
-
+    "*[local-name()='#{name}' and namespace-uri()='#{ns_uri}']"
   end
 
 end

commit 4fb39a6163c398117c414485debbc07a144ace88

Fix the fallout from cbc8f5cf42.
Signed-off-by: Alex Zepeda <alex@inferiorhumanorgans.com>

app/models/carddav/base_resource.rb
@@ -81,9 +81,8 @@ class Carddav::BaseResource < DAV4Rack::Resource
     namespace = element[:ns_href]
 
     begin
-      our_properties = BaseResource.merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
-      our_properties = BaseResource.merge_properties(our_properties, self.class::EXPLICIT_PROPERTIES)
-    rescue
+      our_properties = Carddav::BaseResource.merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
+      our_properties = Carddav::BaseResource.merge_properties(our_properties, self.class::EXPLICIT_PROPERTIES)
       # Just in case we don't have any properties defined on the subclass
       our_properties = BASE_PROPERTIES
     end
@@ -113,7 +112,7 @@ class Carddav::BaseResource < DAV4Rack::Resource
   # Some properties shouldn't be included in an allprop request
   # but it's nice to do some sanity checking so keeping a list is good
   def properties
-    BaseResource::merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
+    Carddav::BaseResource.merge_properties(BASE_PROPERTIES, self.class::ALL_PROPERTIES)
   end
 
   # Properties in alphabetical order